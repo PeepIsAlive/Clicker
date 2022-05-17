73ecc72bb627c99759fb819bcd464c27efb44abc
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2022-05-17 16:49:37 +0700

    adding ui has begun

--- Assets/Scenes/Main/Scripts/ScriptableObject/Configs/StoreElement.cs
+++ Assets/Scenes/Main/Scripts/ScriptableObject/Configs/StoreElement.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class StoreElement : ScriptableObject
 {
+    [SerializeField] private string _name;
+    [SerializeField] private string _description;
+    [SerializeField] private Sprite _image;
+    [SerializeField] private int _price;
 
+    public string Name => _name;
+    public string Description => _description;
+    public Sprite Image => _image;
+    public int Price => _price;
 }
