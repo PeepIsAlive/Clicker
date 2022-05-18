0cc024b46825a5f0a4aaafc6c098993ae45a5b4a
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2022-05-19 02:46:07 +0700

    redesign UI

--- Assets/Scenes/Main/Scripts/ScriptableObject/Configs/StoreElement.cs
+++ Assets/Scenes/Main/Scripts/ScriptableObject/Configs/StoreElement.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New store element", menuName = "Config/StoreElement", order = 52)]
 public class StoreElement : ScriptableObject
 {
+    [SerializeField] private int _id;
     [SerializeField] private string _name;
     [SerializeField] private string _description;
     [SerializeField] private Sprite _image;
     [SerializeField] private int _price;
 
+    public int Id => _id;
     public string Name => _name;
     public string Description => _description;
     public Sprite Image => _image;
